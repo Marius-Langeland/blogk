fd87a341d869f8174b0ba9df81098c7cdd420a3a
Marius <mariuslangeland.ml@gmail.com>
2025-01-11 23:53:21 +0100

    Finished profile template

--- src/lib/component-wide/profile-wide.svelte
+++ src/lib/component-wide/profile-wide.svelte
@@ -4,8 +4,10 @@
 </script>
 
 <div class="profile">
-    <ProfileIcon icon={localStorage.getItem('profile-icon')} size="50px"/>
-    <div class="name"></div>
+    <div class="icon">
+        <ProfileIcon icon={localStorage.getItem('profile-icon')} size="50px"/>
+    </div>
+    <div class="name">{username}</div>
 </div>
 
 <style>
@@ -14,5 +16,16 @@
         display: flex;
         align-items: center;
         padding: .5rem;
+        gap: .5rem;
+    }
+    .icon{
+        background-color: var(--clr_palette_0);
+        border-radius: 50%;
+        padding: .5rem;
+    }
+    .name{
+        color: var(--clr_palette_4);
+        font-size: 20px;
+        font-family: "Playwrite AU SA", serif;
     }
 </style>
--- src/routes/user/[slug]/+page.svelte
+++ src/routes/user/[slug]/+page.svelte
@@ -19,6 +19,33 @@
 <div class="main">
     <div class="content">
         <ProfileWide username={data.username}/>
+        <div class="filters">
+            <button>My collections</button>
+            <button>My public collections</button>
+            <button>My private collections</button>
+        </div>
+
+        <div class="collection">
+            <div class="tags">
+                <h3 class="title">Untitled collection</h3>
+                <span class="privacy">private</span>
+            </div>
+
+            <div class="carousell">
+                <button class="go-left">&lt</button>
+                <ul>
+                    <li></li>
+                    <li>Submission</li>
+                    <li>Submission</li>
+                    <li>Submission</li>
+                    <li>Submission</li>
+                    <li>Submission</li>
+                    <li>Submission</li>
+                    <li>Submission</li>
+                </ul>
+                <button class="go-right">&gt</button>
+            </div>
+        </div>
     </div>
 </div>
 
@@ -36,9 +63,117 @@
         overflow: hidden;
         z-index: 1;
         min-width: 500px;
-        min-height: 200px;
+        max-width: 800px;
+
+        gap: 3px;
 
         display: flex;
         flex-direction: column;
+        backdrop-filter: blur(5px);
+    }
+
+    .tags{
+        display: flex;
+        align-items: center;
+        gap: .2rem;
+        &>*:not(.title){
+            background-color: var(--clr_palette_3);
+            border-radius: 5rem;
+            padding: .1rem .5rem;
+            color: black;
+            font-size: 14px;
+            letter-spacing: 1px;
+            user-select: none;
+        }
+    }
+
+    .filters{
+        display: flex;
+        gap: 3px;
+        &>*{
+            text-align: center;
+            color: var(--clr_transparent_light_50);
+            flex-grow: 1;
+            background-color: var(--clr_transparent_dark_25);
+            padding: .5rem;
+
+            transition: color .2s ease;
+
+            &:hover{
+                color: var(--clr_transparent_light_75);
+            }
+        }
+    }
+
+    .collection{
+        display: flex;
+        gap: 3px;
+        flex-direction: column;
+        &>*{
+            background-color: var(--clr_transparent_dark_25);
+        }
+
+        h3{
+            color: var(--clr_transparent_light_75);
+            margin: 0;
+            padding: .5rem;
+        }
+
+        .carousell{
+            display: flex;
+            padding: .5rem;
+        }
+
+        button{
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            color: var(--clr_transparent_light_50);
+            width: 50px;
+            background-color: var(--clr_transparent_dark_25);
+        }
+
+        .go-left{border-radius: .5rem 0 0 .5rem;}
+        .go-right{border-radius: 0 .5rem .5rem 0;}
+
+        ul{
+            display: flex;
+            flex-direction: row;
+            padding: 0;
+            margin: 0;
+            gap: 1rem;
+            overflow-x: scroll;
+
+            position: relative;
+        }
+
+        li{
+            all: unset;
+            padding: .5rem;
+            height: 120px;
+            aspect-ratio: 5/4;
+            background-color: var(--clr_palette_3);
+            border-radius: .25rem;
+            color: var(--clr_transparent_light_50);
+        }
+
+        li:first-of-type{
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            font-size: 50px;
+
+            &::before{
+                content: '+';
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                background-color: var(--clr_palette_0);
+                color: var(--clr_transparent_light_50);
+                width: 50%;
+                aspect-ratio: 1;
+                border-radius: 50%;
+            }
+        }
     }
 </style>
