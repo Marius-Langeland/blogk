3338501370041d9802cb4439eb3c3f2da9105c45
Marius <mariuslangeland.ml@gmail.com>
2025-01-12 20:01:27 +0100

    created collection overview component

---
+++ src/lib/components/collection-overview.svelte
@@ -0,0 +1,90 @@
+<script lang="ts">
+    let { title = '', privacy = '', submissions = [] } = $props();
+</script>
+
+<div class="collection">
+    <div class="tags">
+        <h3 class="title">{title == '' ? 'Untitled collection' : title}</h3>
+        {#if privacy != ''}
+            <span class="privacy">{privacy}</span>
+        {/if}
+    </div>
+    <div class="grid">
+        <div class="tools">
+            <button>Open collection</button>
+            <button>Add new submission</button>
+        </div>
+        {#each submissions as sub}
+            <div>sub.title</div>
+        {/each}
+    </div>
+</div>
+
+<style>
+    .tags{
+        display: flex;
+        align-items: center;
+        gap: .2rem;
+        &>*:not(.title, button){
+            background-color: var(--clr_palette_3);
+            border-radius: 5rem;
+            padding: .1rem .5rem;
+            color: black;
+            font-size: 14px;
+            letter-spacing: 1px;
+            user-select: none;
+        }
+    }
+
+    .collection{
+        display: flex;
+        gap: 3px;
+        flex-direction: column;
+        
+        &>*{
+            background-color: var(--clr_transparent_dark_25);
+        }
+
+        h3{
+            color: var(--clr_transparent_light_75);
+            margin: 0;
+            padding: .5rem;
+        }
+
+        .grid{
+            display: grid;
+            padding: .5rem;
+            margin: 0;
+            gap: .5rem;
+            
+            grid-template-columns: 1fr 1fr;
+            
+            &>*:not(.tools){
+                all: unset;
+                padding: .5rem;
+                border: 2px solid var(--clr_palette_0);
+                background-color: var(--clr_transparent_dark_25);
+                border-radius: .25rem;
+                color: var(--clr_transparent_light_50);
+                aspect-ratio: 3/1;
+
+                cursor: pointer;
+                user-select: none;
+            }
+        }
+
+        .tools{
+            display: flex;
+            grid-column: 1 / -1;
+            gap: 3px;
+
+            &>*{
+                padding: .5rem;
+                background-color: var(--clr_palette_0);
+                color: var(--clr_transparent_light_50);
+                cursor: pointer;
+                border-radius: .25rem;
+            }
+        }
+    }
+</style>
--- src/routes/user/[slug]/+page.svelte
+++ src/routes/user/[slug]/+page.svelte
@@ -3,6 +3,7 @@
     import { collection, doc, setDoc, getDocs, type QuerySnapshot } from "firebase/firestore";
     import { onMount } from "svelte";
     import ProfileWide from "$lib/component-wide/profile-wide.svelte";
+    import CollectionOverview from "$lib/components/collection-overview.svelte";
     
     let db = getFirebase().firestore;
     const subRef = collection(db, 'collections');
@@ -17,7 +18,7 @@
 
 <div class="main">
     <div class="flex">
-        <div class="content">
+        <div class="content profile">
             <ProfileWide username={data.username}/>
             <div class="filters">
                 <button>My collections</button>
@@ -27,22 +28,7 @@
             </div>
         </div>
         <div class="content">
-            <div class="collection">
-                <div class="tags">
-                    <h3 class="title">Leetcode submissions</h3>
-                    <span class="privacy">public</span>
-                </div>
-                <div class="grid">
-                    <div class="tools">
-                        <button>Open collection</button>
-                        <button>Add new submission</button>
-                    </div>
-                    <div>submission</div>
-                    <div>submission</div>
-                    <div>submission</div>
-                    <div>submission</div>
-                </div>
-            </div>
+            <CollectionOverview/>
         </div>
     </div>
 </div>
@@ -72,20 +58,7 @@
         backdrop-filter: blur(5px);
     }
 
-    .tags{
-        display: flex;
-        align-items: center;
-        gap: .2rem;
-        &>*:not(.title, button){
-            background-color: var(--clr_palette_3);
-            border-radius: 5rem;
-            padding: .1rem .5rem;
-            color: black;
-            font-size: 14px;
-            letter-spacing: 1px;
-            user-select: none;
-        }
-    }
+    
 
     .filters{
         display: flex;
@@ -101,58 +74,6 @@
 
             &:hover{
                 color: var(--clr_transparent_light_75);
-            }
-        }
-    }
-
-    .collection{
-        display: flex;
-        gap: 3px;
-        flex-direction: column;
-        
-        &>*{
-            background-color: var(--clr_transparent_dark_25);
-        }
-
-        h3{
-            color: var(--clr_transparent_light_75);
-            margin: 0;
-            padding: .5rem;
-        }
-
-        .grid{
-            display: grid;
-            padding: .5rem;
-            margin: 0;
-            gap: .5rem;
-            
-            grid-template-columns: 1fr 1fr;
-            
-            &>*:not(.tools){
-                all: unset;
-                padding: .5rem;
-                border: 2px solid var(--clr_palette_0);
-                background-color: var(--clr_transparent_dark_25);
-                border-radius: .25rem;
-                color: var(--clr_transparent_light_50);
-                aspect-ratio: 3/1;
-
-                cursor: pointer;
-                user-select: none;
-            }
-        }
-
-        .tools{
-            display: flex;
-            grid-column: 1 / -1;
-            gap: 3px;
-
-            &>*{
-                padding: .5rem;
-                background-color: var(--clr_palette_0);
-                color: var(--clr_transparent_light_50);
-                cursor: pointer;
-                border-radius: .25rem;
             }
         }
     }
